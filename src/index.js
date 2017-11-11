aa158a5b8aa601eac9bd4ee49fa8536aef86544a
Tailo Mateus Gonsalves <tailogonsalves@gmail.com>
2017-11-11 20:54:15 -0200

    Removing the routes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import PessoaBox from './Pessoa.js';
-import Home from './Home';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-	(
-	<BrowserRouter>
-      <Switch>
-      	<Route path="/" component={App}>
-			<Route path="/" component={Home}/>
-			<Route path="/pessoa" component={PessoaBox}/>
-		</Route>
-      </Switch>
-    </BrowserRouter>), 
-	document.getElementById('root')
+  <PessoaBox/>,
+  document.getElementById('root')
 );
 
 registerServiceWorker();
